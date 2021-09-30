e8c48642e075d83e4ee64d3ae408402c250f87e3
Chon Lok Lei <chonloklei@gmail.com>
2021-09-30 16:28:11 +0800

    Working on sorting tmp

--- design/sort-tmp.py
+++ design/sort-tmp.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 import os
 import glob
+import re
 import argparse
 import numpy as np
+from subprocess import call
 
 parser = argparse.ArgumentParser('Sorting temporary runs.')
 parser.add_argument('-d', '--design', type=str, help='Name of the design.')
@@ -14,5 +16,40 @@ parser.add_argument('-b', '--best_n', type=int, default=5,
 parser.add_argument('--debug', action='store_true')
 args = parser.parse_args()
 
-path = f'./out/{args.design}-{args.mode}-{args.model_name}-tmp'
+run = 0
 
+path = f'./out/{args.design}-{args.mode}-{args.model_name}-tmp'
+new_path = f'./out/{args.design}-{args.mode}-{args.model_name}'
+if not os.path.isdir(new_path):
+    os.makedirs(new_path)
+
+protocols = glob.glob(f'{path}/opt-prt-run{run}-re[0-9]-[0-9].txt')
+scores = glob.glob(f'{path}/opt-prt-score-run{run}-re[0-9]-[0-9].txt')
+assert(type(protocols) is list)
+assert(len(protocols) == len(scores))
+
+score_values = []
+for protocol in protocols:
+    i, j = re.findall(f'{path}/opt-prt-run{run}-re(\d)-(\d).txt', protocol)[0]
+    s = f'{path}/opt-prt-score-run{run}-re{i}-{j}.txt'
+    assert(s in scores)
+    score = np.loadtxt(s)
+    score_values.append(score)
+
+call(['cp',
+      f'{path}/opt-prt-run{run}-re{i}.out',
+      f'{new_path}/opt-prt-run{run}.out'])
+
+call(['cp',
+      f'{path}/opt-prt-run{run}-re{i}-parameter_evaluate.txt',
+      f'{new_path}/opt-prt-run{run}-parameter_evaluate.txt'])
+
+order = np.argsort(score_values)  # (use [::-1] for LL)
+
+for i, o in enumerate(order[:args.best_n]):
+    protocol = protocols[o]
+    score = score_values[o]
+    call(['cp',
+          protocol,
+          f'{new_path}/opt-prt-run{run}-rank{o}.txt'])
+    np.savetxt(f'{new_path}/opt-prt-score-run{run}-rank{o}.txt', [score])
