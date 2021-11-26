d149830ca7c59b1e3d5e560295752096b0484868
Chon Lok Lei <chonloklei@gmail.com>
2021-11-26 17:27:37 +0800

    Working on cross criteria evaluate

--- study/cross-criteria-evaluate.py
+++ study/cross-criteria-evaluate.py
@@ -4,11 +4,7 @@ sys.path.append('..')
 import os
 import numpy as np
 import pints
-
-import model as m
-import score_lsa as s_lsa
-import score_gsa as s_gsa
-import score_shannon as s_sha
+import method.model
 
 #
 # Compute all scores for cross criteria.
@@ -32,22 +28,21 @@ design_list = {
     'GSA-A':(pyoed.GlobalSensitivityDesignMeasure, pyoed.A_criterion),
     'GSA-D':(pyoed.GlobalSensitivityDesignMeasure, pyoed.D_criterion),
     'GSA-E':(pyoed.GlobalSensitivityDesignMeasure, pyoed.Estar_criterion),
-    'Shannon':(ShannonDesignMeasure, [1, 1, 0, 1]),
+#    'Shannon':(ShannonDesignMeasure, [1, 1, 0, 1]),
 }
 
-n_bootstrap_samples = 100  # number of bootstrap model samples
-n_samples = 500  # number of samples to be compared
+n_samples = 512  # number of samples to be compared
 n_steps = 20  # number of steps of the protocol
 dt = 0.1  # ms
 seed_id = 101  # random seed
 
-model_side = ['TNNP', 'Fink', 'OHara', 'Paci', 'BMA', 'Bootstrap']
-measure_side = ['LSA A', 'LSA D', 'LSA E', 'GSA A', 'GSA D', 'GSA E',
-        'Shannon']
+model_side = ['TNNP', 'Fink', 'Grandi', 'O\'Hara', 'CiPA', 'Tomek']
+measure_side = ['LSA A', 'LSA D', 'LSA E', 'GSA A', 'GSA D', 'GSA E',]
+#                'Shannon']
 
-opt_models = ['', '-bma', '-bs']
-opt_measures = ['lsa-A', 'lsa-D', 'lsa-E', 'gsa-sobol-A', 'gsa-sobol-D',
-        'gsa-sobol-E', 'shannon']
+opt_models = ['ohara-2011', 'model-list']
+opt_measures = ['LSA-A', 'LSA-D', 'LSA-E', 'GSA-A', 'GSA-D', 'GSA-E',]
+#                'Shannon']
 
 savedir = './cross-criteria-evaluate'
 if not os.path.isdir(savedir):
@@ -56,139 +51,74 @@ if not os.path.isdir(savedir):
 
 # Create models
 model_list = []
-# Single models
+log_model_list = []
+# NOTE: Transform for GSA, not for LSA!
 for model_file in model_file_list:
-    model_list.append(m.VCModel(model_file, transform=np.exp, dt=dt))
-# BMA
-model_prior = np.ones(len(model_file_list)) / np.float(len(model_file_list))
-model_list.append(m.BMAVCModel(model_file_list, prior=model_prior,
-        transform=np.exp, dt=dt))
-# Bootstrap
-model_list.append(m.BootstrapVCModel(model_file_list,
-        n_samples=n_bootstrap_samples, transform=np.exp, dt=dt))
+    model_list.append(method.model.VCModel(model_file, transform=None, dt=dt, n_steps=n_steps))
+    log_model_list.append(method.model.VCModel(model_file, transform=np.exp, dt=dt, n_steps=n_steps))
 
 # Model parameter bounds
-logp_lower = [-2] * len(m.parameters)  # maybe +/-3
-logp_upper = [2] * len(m.parameters)
+logp_lower = [-2] * len(method.model.parameters)  # maybe +/-3
+logp_upper = [2] * len(method.model.parameters)
 
 
 # protocol parameter: [step_1_voltage, step_1_duration, step_2..., step3...]
 lower = [-120, 50] * n_steps
 upper = [60, 2e3] * n_steps
-boundaries = pints.RectangularBoundaries(lower, upper)
-
 
 # LSA setting
-lsa_local_param = np.ones(model_list[0].n_parameters())
+lsa_local_param = np.ones(len(method.model.parameters))
 lsa_h = 1e-3
-# GSA setting (SALib)
-salib_problem = {
-    'num_vars': model_list[0].n_parameters(),
-    'names': ['p%s' % (i) for i in range(model_list[0].n_parameters())],
-    'bounds': np.array([logp_lower, logp_upper]).T,
-}
-# Shannon measure setting
-weight_sha = [
-    10.,  # shannon_all_measure
-    0.1,  # shannon_segments_measure
-    0.,  # parameter_dependency_measure; see [1] why we drop it.
-    1.,  # output_uncertainty_measure
-]
+# GSA setting
+boundaries = np.array([logp_lower, logp_upper]).T
 
 
 # Create scores shape (model_side, measure_side)
 score_matrix = []
 # Single models
-for model in model_list[:-2]:
+for model in model_list:
     score_list = []
-    # LSA
-    for criterion in criterion_list:
-        score_list.append(
-            s_lsa.LSAScore([model],
-                default_param=lsa_local_param,
-                h=lsa_h,
-                n_steps=n_steps,
-                criterion=criterion)
-        )
-    # GSA
-    for criterion in criterion_list:
-        score_list.append(
-            s_gsa.GSASobolScore([model],
-                problem=salib_problem,
-                n_steps=n_steps,
-                n_samples=n_samples,
-                criterion=criterion)
-        )
-    # Shannon
-    score_list.append(
-        s_sha.ShannonGSAScore([model],
-            problem=salib_problem,
-            n_steps=n_steps,
-            n_samples=n_samples,
-            weight=weight_sha)
-    )
+    for n in design_list:
+        d, c = design_list[n]
+        if 'LSA' in n:
+            sensitivity_method = pyoed.CentralDifferenceSensitivity
+            method_kw = dict(h=h)
+            design = d(model, default_param, criterion=c, method=sensitivity_method,
+                       method_kw=method_kw)
+        elif 'GSA' in n:
+            sensitivity_method = pyoed.SobolFirstOrderSensitivity
+            method_kw = dict(n_samples=n_samples)
+            design = d(model, boundaries, criterion=c, method=sensitivity_method,
+                       method_kw=method_kw)
+            design.set_n_batches(int(n_samples / 2**8))
+        score_list.append(design)
     score_matrix.append(score_list)
-# BMA
+
+# Average model
 score_list = []
-# LSA
-for criterion in criterion_list:
-    score_list.append(
-        s_lsa.LSAScore([model_list[-2]],
-            default_param=lsa_local_param,
-            h=lsa_h,
-            n_steps=n_steps,
-            criterion=criterion)
-    )
-# GSA
-for criterion in criterion_list:
-    score_list.append(
-        s_gsa.GSASobolScore([model_list[-2]],
-            problem=salib_problem,
-            n_steps=n_steps,
-            n_samples=n_samples,
-            criterion=criterion)
-    )
-# Shannon
-score_list.append(
-    s_sha.ShannonGSAScore([model_list[-2]],
-        problem=salib_problem,
-        n_steps=n_steps,
-        n_samples=n_samples,
-        weight=weight_sha)
-)
-score_matrix.append(score_list)
-# Bootstrap
-score_list = []
-# LSA
-for criterion in criterion_list:
-    score_list.append(
-        s_lsa.LSAScore_Bootstrap(model_list[-1],
-            default_param=lsa_local_param,
-            h=lsa_h,
-            n_steps=n_steps,
-            criterion=criterion)
-    )
-# GSA
-for criterion in criterion_list:
-    score_list.append(
-        s_gsa.GSASobolScore_Bootstrap(model_list[-1],
-            problem=salib_problem,
-            n_steps=n_steps,
-            n_samples=n_samples,
-            criterion=criterion)
-    )
-# Shannon
-score_list.append(
-    s_sha.ShannonGSAScore_Bootstrap(model_list[-1],
-        problem=salib_problem,
-        n_steps=n_steps,
-        n_samples=n_samples,
-        weight=weight_sha)
-)
+for n in design_list:
+    average_list = []
+    d, c = design_list[n]
+    if 'LSA' in n:
+        sensitivity_method = pyoed.CentralDifferenceSensitivity
+        method_kw = dict(h=h)
+        for model in model_list:
+            design = d(model, default_param, criterion=c, method=sensitivity_method,
+                       method_kw=method_kw)
+            average_list.append(design)
+    elif 'GSA' in n:
+        sensitivity_method = pyoed.SobolFirstOrderSensitivity
+        method_kw = dict(n_samples=n_samples)
+        for model in model_list:
+            design = d(model, boundaries, criterion=c, method=sensitivity_method,
+                       method_kw=method_kw)
+            design.set_n_batches(int(n_samples / 2**8))
+            average_list.append(design)
+    score_list.append(pyoed.CombineDesignMeasure(average_list, aggregate=np.mean))
 score_matrix.append(score_list)
 
 
-# Get optimal protocols and evaluate the scores
+# Get optimal protocols and evaluate the scores TODO
 for opt_model in opt_models:
     for opt_measure in opt_measures:
         # Load protocol
