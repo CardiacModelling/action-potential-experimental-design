3a906b5b80d674cd7e2e8dcd518ad56b146b47e2
Chon Lok Lei <chonloklei@gmail.com>
2021-12-16 16:28:25 +0800

    Allow splitting run with option --tmp

--- design/optimise-cc-single.py
+++ design/optimise-cc-single.py
@@ -9,6 +9,11 @@ import argparse
 import numpy as np
 import pints
 import pyoed
+import gc
+gc.enable()
+
+import psutil
+print(psutil.virtual_memory())
 
 import method.model
 
@@ -33,12 +38,31 @@ parser.add_argument('-d', '--design', type=str,
 parser.add_argument('-l', '--model_file', help='A mmt model file.')
 parser.add_argument('-n', '--n_optim', type=int, default=3,
     help='Number of optimisation repeats.')
+parser.add_argument('-r', '--repeat_id', type=int, default=0,
+    help='Repeat ID for `--tmp`, splitting a full run into multiple runs.')
+parser.add_argument('-p', '--parallel', type=int, default=-1,
+    help='Enables/disables parallel evaluation for PINTS.' \
+	 + ' Set -1 to use all CPU. Set 0 to disable parallel evaluation.')
+parser.add_argument('--rand_x0', action='store_true',
+    help='Randomly initialise the optimisation starting point.')
+parser.add_argument('--tmp', action='store_true',
+    help='Output to tmp folder before postprocessing.')
 parser.add_argument('--debug', action='store_true')
 args = parser.parse_args()
 
 model_name = os.path.splitext(os.path.basename(args.model_file))[0]
 
+if args.parallel == -1:
+    set_parallel = True
+else:
+    set_parallel = args.parallel
+
 savedir = './out/' + args.design + '-cc-' + model_name
+if args.tmp:
+    savedir += '-tmp'
+    seed_id += args.repeat_id  # Each repeat run has a different seed.
+    run_id = str(run_id) + '-re' + str(args.repeat_id)
+
 if not os.path.isdir(savedir):
     os.makedirs(savedir)
 
@@ -52,30 +76,38 @@ if 'LSA' in args.design:
     default_param = np.ones(len(method.model.parameters))
 elif 'GSA' in args.design or 'Shannon' in args.design:
     transform = np.exp
-    n_samples = 1024
+    n_samples = 512
     logp_lower = [-2] * len(method.model.parameters)  # maybe +/-3
     logp_upper = [2] * len(method.model.parameters)
 
 # Create model
-model = method.model.CCModel(args.model_file, transform=transform, dt=dt, n_steps=n_steps)
+model = method.model.CCModel(
+    args.model_file,
+    transform=transform,
+    dt=dt,
+    n_steps=n_steps,
+    max_evaluation_time=10,
+)
 
 # Protocol parameter: [holding_1_duration, holding_2..., ...]
 lower = [50] * (n_steps - 1)
 upper = [2e3] * (n_steps - 1)
 boundaries = pints.RectangularBoundaries(lower, upper)
+transformation = pints.RectangularBoundariesTransformation(boundaries)
 
 # Create design
 d, c = design_list[args.design]
 if 'LSA' in args.design:
-    method = pyoed.CentralDifferenceSensitivity
+    sensitivity_method = pyoed.CentralDifferenceSensitivity
     method_kw = dict(h=h)
-    design = d(model, default_param, criterion=c, method=method,
+    design = d(model, default_param, criterion=c, method=sensitivity_method,
                method_kw=method_kw)
 elif 'GSA' in args.design:
-    method = pyoed.SobolFirstOrderSensitivity
+    sensitivity_method = pyoed.SobolFirstOrderSensitivity
     method_kw = dict(n_samples=n_samples)
     b = np.array([logp_lower, logp_upper]).T
-    design = d(model, b, criterion=c, method=method, method_kw=method_kw)
+    design = d(model, b, criterion=c, method=sensitivity_method,
+               method_kw=method_kw)
 elif 'Shannon' in args.design:
     design = None
     raise NotImplementedError
@@ -108,7 +140,7 @@ if args.debug:
     fname = savedir + '/design-profile.prof'
     print('Benchmarking design.__call__')
     # Testing protocol parameters
-    x0 = [1000, 800, 500, 400, 1000] * 4
+    x0 = [1000, 800, 500, 400, 1000] * (n_steps // 5)
     x0 = x0[:-1]
     cProfile.run('x = design(x0)', fname)
     p = pstats.Stats(fname)
@@ -147,6 +179,7 @@ with open('%s/%s-run%s.out' % (savedir, prefix, run_id), 'w') as f:
     f.write('\np_evaluate_file = "' + p_evaluate_file + '"')
     f.write('\nn_steps = ' + str(n_steps))
     f.write('\ndt = ' + str(dt))
+    f.write('\nrand_x0 = ' + str(args.rand_x0))
     f.write('\nseed_id = ' + str(seed_id))
     f.write('\nfit_seed = ' + str(fit_seed))
     f.write('\nProtocol lower bound:\n    ')
@@ -159,13 +192,20 @@ with open('%s/%s-run%s.out' % (savedir, prefix, run_id), 'w') as f:
 # Optimise design
 params, scores = [], []
 
-for _ in range(args.n_optim):
+for i_optim in range(args.n_optim):
     # Get x0
-    need_x0 = True
-    while need_x0:
-        x0 = boundaries.sample(1)[0]
-        if np.isfinite(design(x0)):
-            need_x0 = False
+    if args.rand_x0:
+        need_x0 = True
+        while need_x0:
+            x0 = boundaries.sample(1)[0]
+            if np.isfinite(design(x0)):
+                need_x0 = False
+    else:
+        x0 = [100, 300, 1000, 500, 400] * (n_steps // 5)
+        x0 = x0[:-1]
+
+    optimiser = pints.CMAES
+
     print('x0: ', x0)
 
     # Try it with x0
@@ -176,11 +216,13 @@ for _ in range(args.n_optim):
     opt = pints.OptimisationController(
             design,
             x0,
-            boundaries=boundaries,
-            method=pints.CMAES)
-    opt.set_max_iterations(None)
+            # boundaries=boundaries,  # transformation will handle the bounds.
+            transformation=transformation,
+            method=optimiser)
+    opt.optimiser().set_population_size(30)
+    opt.set_max_iterations(1000)
     opt.set_max_unchanged_iterations(iterations=100, threshold=1e-3)
-    opt.set_parallel(True)
+    opt.set_parallel(set_parallel)
 
     # Run optimisation
     try:
@@ -198,35 +240,41 @@ for _ in range(args.n_optim):
         traceback.print_exc()
         raise RuntimeError('Not here...')
 
-# Order from best to worst
-order = np.argsort(scores)  # (use [::-1] for LL)
-scores = np.asarray(scores)[order]
-params = np.asarray(params)[order]
+    if args.tmp:
+        fn = '%s/%s-run%s-%s.txt' % (savedir, prefix, run_id, i_optim)
+        utils.save_protocol(fn, p, mode='cc')
+        gn = '%s/%s-score-run%s-%s.txt' % (savedir, prefix, run_id, i_optim)
+        np.savetxt(gn, np.asarray(s).reshape(-1))
 
-# Show results
-bestn = min(5, args.n_optim)
-print('Best %d scores:' % bestn)
-for i in range(bestn):
-    print(scores[i])
-print('Mean & std of logposterior:')
-print(np.mean(scores))
-print(np.std(scores))
-print('Worst logposterior:')
-print(scores[-1])
+    del(opt)
+    gc.collect()
 
-#
-# Store bestn results
-#
-obtained_scores = scores[:3]
-obtained_parameters = params[:3]
-for i in range(bestn):
-    p = obtained_parameters[i]
-    fn = '%s/%s-run%s-rank%s.txt' % (savedir, prefix, run_id, i)
-    with open(fn, 'w') as f:
-        f.write('# Holding duration [ms]\n')
-        for i in range(len(p)):
-            f.write(pints.strfloat(p[i]) \
-                    + '\n' \
-                    )
+if not args.tmp:
+    # Order from best to worst
+    order = np.argsort(scores)  # (use [::-1] for LL)
+    scores = np.asarray(scores)[order]
+    params = np.asarray(params)[order]
+
+    # Show results
+    bestn = min(5, args.n_optim)
+    print('Best %d scores:' % bestn)
+    for i in range(bestn):
+        print(scores[i])
+    print('Mean & std of logposterior:')
+    print(np.mean(scores))
+    print(np.std(scores))
+    print('Worst logposterior:')
+    print(scores[-1])
+
+    #
+    # Store bestn results
+    #
+    obtained_scores = scores[:bestn]
+    obtained_parameters = params[:bestn]
+    for i in range(bestn):
+        fn = '%s/%s-run%s-rank%s.txt' % (savedir, prefix, run_id, i)
+        utils.save_protocol(fn, obtained_parameters[i], mode='cc')
+        gn = '%s/%s-score-run%s-rank%s.txt' % (savedir, prefix, run_id, i)
+        np.savetxt(gn, np.asarray(obtained_scores[i]).reshape(-1))
 
 print('Done')
